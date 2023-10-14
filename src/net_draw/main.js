09177af2c14d7c73c412e7c4b68fba8d3bde119b
Guillaume Yziquel <guillaume.yziquel@mailfence.com>
2023-10-14 23:42:40 +0200

    First shot at reading YAML data.

---
+++ src/net_draw/main.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node read-yaml.js <filename>');
+  process.exit(1);
+}
+
+const filename = process.argv[2];
+
+try {
+  const yamlData = fs.readFileSync(filename, 'utf8');
+  const parsedData = yaml.safeLoad(yamlData);
+  draw(parsedData)
+} catch (err) {
+  console.error(`Error reading or parsing the YAML file: ${err.message}`);
+  process.exit(1);
+}
